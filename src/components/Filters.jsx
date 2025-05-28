0cc3377517b08a8792e6fbb676f1564bb3df201a
mhdhssn <mhdhssn967@gmail.com>
2025-05-28 18:35:10 +0530

    Margin adjustments

--- src/components/Filters.jsx
+++ src/components/Filters.jsx
@@ -12,43 +12,51 @@ const Filters = ({
 
   return (
     <div className='filter-div'>
-      <label htmlFor="searchFilter">Search </label>
-      <input
-        placeholder='Search remarks, amount'
-        type="text"
-        id="searchFilter"
-        onChange={(e) => setSearchText(e.target.value)}
-      />
+      <div className='search-filter'>
+        <label htmlFor="searchFilter"><i className="fa-solid fa-magnifying-glass"></i> </label>
+        <input
+          placeholder='Search remarks, amount'
+          type="text"
+          id="searchFilter"
+          onChange={(e) => setSearchText(e.target.value)}
+        />
+      </div>
 
       {/* Dynamically generate filter dropdowns from preferences.fields */}
-      {Object.entries(fields).map(([fieldKey, options]) => (
-        <React.Fragment key={fieldKey}>
-          <label htmlFor={`${fieldKey}-filter`}>{fieldKey} </label>
-          <select
-            id={`${fieldKey}-filter`}
-            onChange={(e) =>
-              setFilterValues({ ...filterValues, [`${fieldKey}`]: e.target.value })
-            }
-          >
-            <option value="All">All</option>
-            {options.map((option) => (
-              <option key={option} value={option}>
-                {option}
-              </option>
-            ))}
-          </select>
-        </React.Fragment>
-      ))}
+      <div className='filter-select-div'>
+        {Object.entries(fields).map(([fieldKey, options]) => (
+          <React.Fragment key={fieldKey}>
+            <div className='filter-selects'>
+              <label htmlFor={`${fieldKey}-filter`}>{fieldKey} </label>
+              <select
+                id={`${fieldKey}-filter`}
+                onChange={(e) =>
+                  setFilterValues({ ...filterValues, [`${fieldKey}`]: e.target.value })
+                }
+              >
+                <option value="All">All</option>
+                {options.map((option) => (
+                  <option key={option} value={option}>
+                    {option}
+                  </option>
+                ))}
+              </select>
+            </div>
+          </React.Fragment>
+        ))}
+      </div>
 
-      <label htmlFor="sort-by">Sort By </label>
-      <select
-        id="sort-by"
-        onChange={(e) => setSortBy(e.target.value)}
-      >
-        <option value='date'>Date incurred</option>
-        <option value='amount'>Amount</option>
-        <option value='createdAt'>Date added</option>
-      </select>
+      <div className='sort-by-div'>
+        <label htmlFor="sort-by">Sort By </label>
+        <select
+          id="sort-by"
+          onChange={(e) => setSortBy(e.target.value)}
+        >
+          <option value='date'>Date incurred</option>
+          <option value='amount'>Amount</option>
+          <option value='createdAt'>Date added</option>
+        </select>
+      </div>
     </div>
   );
 };
