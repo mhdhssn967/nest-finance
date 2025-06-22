e04e8240bf0d520e75dd2bb2e5b8d301bb9692b5
mhdhssn <mhdhssn967@gmail.com>
2025-06-22 19:47:13 +0530

    Add image eror fixed

--- src/components/Navigator.jsx
+++ src/components/Navigator.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Link, useLocation } from 'react-router-dom'
 import './Navigator.css'
+import img from '../assets/image.png'
 
 
 const Navigator = ({preferences,companyDetails}) => {
@@ -14,7 +15,7 @@ const location=useLocation()
         <Link to={'/home/insights'}><button className={`default-btn ${location.pathname === '/home/insights' ? 'active' : ''}`}>Insights</button></Link>
       </div>
       <div className='comp-div'>
-        <img className='company-logo' src={companyDetails.logo} alt="" />
+        <img className='company-logo' src={companyDetails?companyDetails.logo:img} alt="" />
                 <h1 className='comp-name' style={{ fontWeight:'800'}}>{preferences&&preferences.cName || "Loading..."}</h1>
       </div>
     <div></div>
